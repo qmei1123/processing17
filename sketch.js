11551d83a599a8687d27d6303a5d529dfc3ade91
qmei1123 <qmei@bu.edu>
2018-02-25 16:16:01 -0500

    make it rain

--- sketch.js
+++ sketch.js
@@ -1,35 +1,43 @@
 var upLine = []; // array of Jitter objects
-var x;
+
 function setup() {
-  createCanvas(1000, 900);
+  createCanvas(1275, 680);
   // Create objects
   for (var i=0; i<50; i++) {
     upLine.push(new movement());
-    x = width/2 + random(-15, 15);
   }
 }
 
 function draw() {
-  background(50, 89, 100);
-  for (var i=0; i< 10; i++) {
+  background(20, 20, 40);
+  for (var i=0; i<upLine.length; i++) {
     upLine[i].move();
     upLine[i].display();
   }
 }
 
-
 // Jitter class
 function movement() {
+  this.x = random(width);
   this.y = random(height);
   this.yTwo = this.y + random(50);
   this.speed = 1;
 
   this.move = function() {
+    this.x = random(width);
     this.y += random(-this.speed, this.speed);
   };
 
   this.display = function() {
-    line(x, this.y, x, this.yTwo);
+      strokeWeight(5);
+    stroke(15, 15, 70);
+    line(this.x, this.y, this.x, this.yTwo);
   };
+}
+
+function bar() {
+    this.display = function() {
+        rect()
+    }
 }
 
